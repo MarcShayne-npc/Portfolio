9c8359d7fcd5b89e73e10dffd2a74293d5685a53
MarcShayne <74227672+MarcShayne-npc@users.noreply.github.com>
2024-04-07 00:56:12 +0800

    Change constants project

--- src/constants/index.js
+++ src/constants/index.js
@@ -220,8 +220,8 @@ const projects = [
   },
   {
     id: 'project-4',
-    name: 'Movie Metro',
-    description: `A single-page application that allows users to search for any movie or show's ratings and its details.`,
+    name: 'Coming soon...',
+    description: ``,
     tags: [
       {
         name: 'nextjs',
@@ -236,14 +236,14 @@ const projects = [
         color: 'pink-text-gradient',
       },
     ],
-    image: movie,
+    image: avalon,
     repo: '',
   },
   {
     id: 'project-5',
-    name: 'Nyeusi Fest Site',
+    name: 'Coming soon...',
     description:
-      'This is a demo concert website for a music festival called Nyeusi.',
+      '',
     tags: [
       {
         name: 'nextjs',
@@ -258,7 +258,7 @@ const projects = [
         color: 'pink-text-gradient',
       },
     ],
-    image: nyeusi,
+    image: avalon,
     repo: '',
     
   },
